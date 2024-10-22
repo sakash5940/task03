13341d2d960bb0712ae9ae168ac1e152cced6606
sakash5940 <akashpatil5940@gmail.com>
2024-10-22 19:18:45 +0530

    Add files via upload

---
+++ task03.js
@@ -0,0 +1,25 @@
+//write a program to print the array in reverse order
+//input:-- [1,2,3,4,5,{id:1,age:25},[1,2,3]]
+//ouput:-- [[1,2,3],{id:1,age:25},5,4,3,2,1]
+var str=[1,2,3,4,5,{id:1,age:25},[1,2,3]]
+var reversestr=""
+for(i=str.length-1;i>=0;i--){
+    reversestr=reversestr+str[i]
+    console.log(str[i])
+}
+    console.log(reversestr)
+    //question2
+    var str="akash";
+      var reversestr=""
+     for(i=str.length-1;i>=onabort;i--){
+        reversestr=reversestr+str[i]
+     }
+     console.log(reversestr)
+
+     //question3
+     //write a program to print the age in the given array
+     //input:-- [1,2,3,4,5,{id:1,age:25},[1,2,3]]
+       //output:-- 25
+       
+   
+
